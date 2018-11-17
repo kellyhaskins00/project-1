0e384095f1025d70a1da1885090bc4f832f24bf2
B230ft <igorniyazov@gmail.com>
2018-11-17 14:04:16 -0800

    working on api

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+$("button").on("click", function() {
+    var map;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
