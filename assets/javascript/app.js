fbf2034871b3a52e82424fb76e1faac59a66f0ef
Kelly Haskins <kellyhaskins00@gmail.com>
2018-11-19 17:59:54 -0800

    updated html css and logo

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
-$("button").on("click", function() {
+/*$("button").on("click", function() {
     var map;
     function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
         zoom: 8
       });
-    }
+    }*/
 
 
 
